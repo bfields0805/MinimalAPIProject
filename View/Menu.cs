eea72f5742f83405f6f573fba594c773bd0e5d93
Bryon F <bryon790@revature.net>
2022-11-01 16:36:56 -0500

    model and view skeleton

---
+++ View/Menu.cs
@@ -0,0 +1,16 @@
+namespace View
+{
+    public class Menu
+    {
+        public Menu()
+        {
+
+        }
+
+        private int loginMenu()
+        {
+        
+
+        }
+    }
+}
---
+++ View/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Model;
+
+namespace view{
+public class Program
+{
+   public static void main(string[] args)
+   {
+     Employee admin = new Employee("admin@admin.com", "admin", true);
+
+   }
+}
+} 
